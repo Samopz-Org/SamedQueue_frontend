96940a9bf4ceca8d3971e83fba8069edcbf969bd
Samson Adewumi <samopzz@gmail.com>
2024-11-10 01:08:25 +0100

    Update login.js

--- src/components/auth.js/login.js
+++ src/components/auth.js/login.js
@@ -40,7 +40,7 @@ const Login = ({ setAuthenticated }) => {
 
   return (
     <div>
-      <h3>Already have an account? Login in 👇!</h3>
+      <h3>Already have an account? Log in 👇!</h3>
       <form onSubmit={handleLogin}>
         <div>
           <label for="username" ></label>
