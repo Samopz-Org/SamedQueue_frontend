ed9d5347adba4ba2e3c338f15dd278e27701db20
Samson Adewumi <samopzz@gmail.com>
2025-03-25 12:01:25 +0000

    updates

--- src/components/auth/home.js
+++ src/components/auth/home.js
@@ -54,14 +54,14 @@ const Home = () => {
               "Samopz' Clinic provided exceptional care. The doctors were
               attentive and compassionate."
             </p>
-            <h4>- Jane Doe</h4>
+            <h4>- Hannah Badmus</h4>
           </div>
           <div className="testimonial">
             <p>
               "I felt valued and cared for. The staff went above and beyond to
               ensure my comfort."
             </p>
-            <h4>- John Smith</h4>
+            <h4>- David Douglas</h4>
           </div>
         </div>
       </section>
